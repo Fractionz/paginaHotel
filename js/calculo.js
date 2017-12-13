59919c4bc6e12d8ae75cee58ac0146815739f0bc
Chuy <kendo_keita@outlook.com>
2017-12-12 21:22:29 -0700

    agregamis habis

---
+++ js/calculo.js
@@ -0,0 +1,53 @@
+
+function calcularC1(){
+    
+    var dias = document.getElementById("dias").value;
+    var costo = 2500;
+    valor=parseInt(dias)*parseInt(costo);
+    document.getElementById("total").innerHTML=valor;
+}
+
+function calcularC2(){
+    
+    var dias = document.getElementById("dias").value;
+    var costo = 3500;
+    valor=parseInt(dias)*parseInt(costo);
+    document.getElementById("total").innerHTML=valor;
+}
+
+
+function calcularC3(){
+    
+    var dias = document.getElementById("dias").value;
+    var costo = 4500;
+    valor=parseInt(dias)*parseInt(costo);
+    document.getElementById("total").innerHTML=valor;
+}
+
+
+function calcularH1(){
+    
+    var dias = document.getElementById("dias").value;
+    var costo = 4200;
+    valor=parseInt(dias)*parseInt(costo);
+    document.getElementById("total").innerHTML=valor;
+}
+
+
+function calcularH2(){
+    
+    var dias = document.getElementById("dias").value;
+    var costo = 3500;
+    valor=parseInt(dias)*parseInt(costo);
+    document.getElementById("total").innerHTML=valor;
+}
+
+
+function calcularH3(){
+    
+    var dias = document.getElementById("dias").value;
+    var costo = 4000;
+    valor=parseInt(dias)*parseInt(costo);
+    document.getElementById("total").innerHTML=valor;
+}
+
---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode:'fade',
+        captions: true,
+        slideWidth:600,
+        
+        auto:true,
+        autoControls:true,
+        stopAutoOnclick:true,
+        speed:100,
+        pause:4000,
+        keyboardEnabled:true
+    });
+});
